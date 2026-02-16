6baba2fd62a32e9eca77966e9beb82821afdd38d
Ryan <mccann.ryan.92@gmail.com>
2026-02-15 20:35:15 -0600

    Added dependencies for crypto and path.

--- app/server.js
+++ app/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const path = require('path');
 const dbConfig = require('./dbConfig');
 
 const app = express();
