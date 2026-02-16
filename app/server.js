1df71983d8230b24e9bd3cef5aa7e5f1d4f66d0a
Ryan <mccann.ryan.92@gmail.com>
2026-02-15 20:13:47 -0600

    Added db access via dbConfig which uses a .env file to store credentials. Added routes to login, register, and access new-tech page.

--- app/server.js
+++ app/server.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
+const dbConfig = require('./dbConfig');
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const db = mysql.createConnection({
-    host: host,
-    user: user,
-    password: password,
-    database: db
-});
+const db = mysql.createConnection(dbConfig);
 
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'login.html'));
@@ -20,3 +16,8 @@ app.get('/register', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'register.html'));
 });
 
+app.get('/new-tech', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'new-tech.html'));
+})
+
+app.post('/login')
