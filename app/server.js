e62d2ef875b3f2c3ea3d2a20b15f14c9a2f53c39
Ryan <mccann.ryan.92@gmail.com>
2026-02-23 20:32:31 -0600

    Finished login code and added code to allow responding to poll.

--- app/server.js
+++ app/server.js
@@ -5,16 +5,26 @@ const crypto = require('crypto');
 const path = require('path');
 const dbConfig = require('./dbConfig');
 const fs = require('fs');
+const session = require('express-session');
 
 // Loading sql files here
 const register_sql = fs.readFileSync('./db/create_user.sql', 'utf8');
 const get_user_sql = fs.readFileSync('./db/get_user.sql', 'utf8');
+const create_vote_sql = fs.readFileSync('./db/create_vote.sql', 'utf8');
+const get_votes_sql = fs.readFileSync('./db/get_votes.sql', 'utf8');
+const get_user_voted_sql = fs.readFileSync('./db/get_user_voted.sql', 'utf8');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'change-this-later',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }
+}));
 
 const db = mysql.createConnection(dbConfig);
 
@@ -80,14 +90,88 @@ app.post('/login', (req, res) => {
         }
 
         if(results.length > 0) {
-            if(results[0].Password_Hash === hashedPassword && results[0].Username === username)
+            if(results[0].Password_Hash === hashedPassword && results[0].Username === username) {
+                req.session.userId = results[0].UserID;
+                req.session.username = results[0].Username;
                 res.json( {success: true} );
+            }
             else
                 res.json( {error: 'Invalid password.'} );
         } else {
             res.json( {error: 'Invalid username.'} );
         }
     });
+});
+
+app.get('/voted', (req, res) => {
+    const poll = req.query.PollID;
+
+    if(req.session.userId) {
+        db.query(get_user_voted_sql, [req.session.userId, poll], (err, results) => {
+            if(err) {
+                console.error(err);
+                return res.status(500).json({error: 'Server error'});
+            }
+            if(results.length > 0) {
+                db.query(get_votes_sql, [poll], (err, results) => {
+                    if(err) {
+                        console.error(err);
+                        return res.status(500).send('Server error');
+                    }
+
+                    res.json({
+                        voted: true,
+                        results: results
+                    });
+                });
+            } else {
+                return res.json({voted: false});
+            }
+        });
+    } else {
+        res.json({voted: false});
+    }
+});
+
+app.post('/votepoll', (req, res) => {
+    const poll = req.body.PollID;
+    const option = req.body.OptionID;
+
+    if(req.session.userId) {
+        db.query(create_vote_sql, [poll, option, req.session.userId], (err, results) => {
+            if(err) {
+                if(err.code === 'ER_DUP_ENTRY') {
+                    db.query(get_votes_sql, [poll], (err, results) => {
+                        if(err) {
+                            console.error(err);
+                            return res.status(500).send('Server error');
+                        }
+
+                        return res.json({
+                            results: results,
+                            message: "You already voted."
+                        });
+                    });
+                } else {
+                    console.error(err);
+                    return res.status(500).send('Server error');
+                }
+            }
+
+            db.query(get_votes_sql, [poll], (err, results) => {
+                if(err) {
+                    console.error(err);
+                    return res.status(500).send('Server error');
+                }
+
+                res.json({
+                    results: results
+                });
+            });
+        });
+    } else {
+        res.json( {error: "You must be logged in to vote."} );
+    }
 });
 
 app.listen(port, () => {
