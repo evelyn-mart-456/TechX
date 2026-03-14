a96a2ea52bd7203039def805c4b4ed8bb895be88
MGertonsonSXU <gertonson.m01@mymail.sxu.edu>
2026-03-13 22:28:41 -0500

    changing the config and env

--- Sprint 2/dbConfig.js
+++ Sprint 2/dbConfig.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT
 };
