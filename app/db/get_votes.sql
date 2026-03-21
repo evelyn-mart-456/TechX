e1e547a1bc94ef7f92795646bc29d6a3b2696702
evelyn-mart-456 <martinez.e37@mymail.sxu.edu>
2026-03-21 09:41:55 -0500

    my local changes

--- app/db/get_votes.sql
+++ app/db/get_votes.sql
@@ -1,4 +1,7 @@
-SELECT OptionName, Votes
-FROM polloptions
-WHERE PollID = ?;
+Select PollVotes.OptionID, COUNT (PollVotes.OptionID) AS 
+Votes, polloptions.OptionName AS OptionName
+From PollVotes
+Join Polloptions on PollVotes.OptionID = polloptions.OptionID
+Where PollVotes.PollID = ?
+Group By PollVotes.OptionID, polloptions.OptionName
 
