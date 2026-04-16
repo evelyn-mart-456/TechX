837f8e18c22461c51bd1c16f81c547c61daa2699
MGertonsonSXU <gertonson.m01@mymail.sxu.edu>
2026-04-16 10:47:30 -0500

    fixing the create order/ trying to unify the techx navbar

--- app/db/create_order.sql
+++ app/db/create_order.sql
@@ -1,2 +1,12 @@
 INSERT INTO orders (UserID, OrderNumber, Total, ShippingID, CreatedAt)
-VALUES (?, (SELECT COALESCE(MAX(OrderNumber), 0) + 1 FROM orders WHERE UserID = ?), ?, ?, NOW());
+SELECT
+    '4d2c39c4-89cf-4dcc-94cb-6ad0e95cd7d2',
+    COALESCE(MAX(OrderNumber), 0) + 1,
+    2999,
+    1,
+    NOW()
+FROM (
+    SELECT OrderNumber 
+    FROM orders 
+    WHERE UserID = '4d2c39c4-89cf-4dcc-94cb-6ad0e95cd7d2'
+) AS t;
